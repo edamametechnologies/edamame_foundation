212a3084fefa069d38bbc21d62f9c8590eb79ce0
franklyonnet <flyonnet@edamame.tech>
2023-12-16 16:51:58 +0100

    ADD: download key DBs from Github

--- src/lanscan_profile.rs
+++ src/lanscan_profile.rs
@@ -1,6 +1,6 @@
 use std::collections::HashSet;
 use serde::{Deserialize, Serialize};
-use log::{info, trace};
+use log::{info, trace, error};
 // Tokio Mutex
 use tokio::sync::Mutex;
 use once_cell::sync::Lazy;
@@ -12,6 +12,7 @@ use crate::lanscan_types::*;
 pub struct DeviceTypeRule {
     device_type: String,
     open_ports: Vec<u16>,
+    mdns_services: Vec<String>,
     vendors: Vec<String>,
     os_list: Vec<String>,
 }
@@ -36,8 +37,8 @@ static PROFILES: Lazy<Mutex<DeviceTypeList>> = Lazy::new(|| {
 
 // TODO: also use the discovered services to determine the device type (for example "Deskjet" in the case of a printer)
 
-pub async fn device_type(port_info: &Vec<PortInfo>, oui_vendor: &str, os_name: &str) -> String {
-    trace!("Computing device type for {:?} ports, {} vendor, {} OS", port_info, oui_vendor, os_name);
+pub async fn device_type(port_info: &Vec<PortInfo>, mdns_services: &Vec<String>, oui_vendor: &str, os_name: &str) -> String {
+    trace!("Computing device type for ports {:?}, mdns {:?}, vendor {}, OS name {}", port_info, mdns_services, oui_vendor, os_name);
 
     trace!("Locking PROFILES - start");
     let device_types = PROFILES.lock().await;
@@ -47,25 +48,40 @@ pub async fn device_type(port_info: &Vec<PortInfo>, oui_vendor: &str, os_name: &
     let open_ports_set: HashSet<u16> = port_info.iter().map(|info| info.port).collect();
 
     for rule in device_types.device_types.iter() {
-        // Check all mandatory ports are here, unless the list is empty
-        if rule.open_ports.is_empty() || rule.open_ports.iter().all(|port| open_ports_set.contains(port)) {
+
+        // Match on ports, a type is defined by a unique set of ports
+        if ! rule.open_ports.is_empty() && rule
+            .open_ports
+            .iter()
+            .all(|port| open_ports_set.contains(port)) {
             trace!("Match for ports {:?} : {:?}", rule.open_ports, rule);
-            // Check at least one vendor is here, unless the list is empty
-            if rule.vendors.is_empty() || rule
-                .vendors
-                .iter()
-                .any(|brand| oui_vendor_lower.contains(brand))
-            {
-                trace!("Match for vendors {:?} : {:?}", rule.vendors, rule);
-                // Use the OS as sanity check unless the provided OS name is empty
-                if os_name.is_empty() || rule.os_list.contains(&os_name.to_owned()) {
-                    trace!("Match for os {:?} : {:?}", rule.os_list, rule);
-                    trace!("Found device type {:?}", rule.device_type);
-                    return rule.device_type.to_string();
-                }
-            }
+            return rule.device_type.to_string();
+        };
+
+        // Match on vendors, a type is defined by the presence of at least one vendor
+        if ! rule.vendors.is_empty() && rule
+            .vendors
+            .iter()
+            .any(|brand| oui_vendor_lower.contains(brand))
+        {
+            trace!("Match for vendors {:?} : {:?}", rule.vendors, rule);
+            return rule.device_type.to_string();
         }
+
+        // Match on services, a type is defined by the presence of at least one service
+        if ! rule.mdns_services.is_empty() && rule
+            .mdns_services
+            .iter()
+            .any(|service| mdns_services.contains(service))
+        {
+            trace!("Match for services {:?} : {:?}", rule.mdns_services, rule);
+            return rule.device_type.to_string();
+        }
+
+        // We don't use os name for now
     }
+    // Generate an error to be able to see if we missed a device type
+    error!("Unknown device type for ports {:?}, mdns {:?}, vendor {}, OS name {}", port_info, mdns_services, oui_vendor, os_name);
 
     "Unknown".to_string()
 }
