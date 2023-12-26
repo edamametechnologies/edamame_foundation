ff7aef0c29056d20998acec5dfb6d969570f36cb
Frank Lyonnet <flyonnet@edamame.tech>
2023-12-26 22:37:44 +0100

    Minor changes

--- src/lanscan_types.rs
+++ src/lanscan_types.rs
@@ -3,7 +3,6 @@ use crate::lanscan_port_vulns::*;
 
 #[derive(Debug, Serialize, Deserialize, Clone, Ord, Eq, PartialEq, PartialOrd)]
 pub struct PortInfo {
-
     pub port: u16,
     pub protocol: String,
     pub service: String,
