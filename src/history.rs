7cb6540e3d3e772f32c652b6fecf1e9e71448d8c
Frank Lyonnet <flyonnet@edamame.tech>
2024-02-04 19:55:14 +0100

    ADD: extension of edamame_foundation for use with the backend

--- src/history.rs
+++ src/history.rs
@@ -1,7 +1,7 @@
 use serde::{Serialize, Deserialize};
 use crate::order_backend::*;
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
 pub struct OrderHistory {
     pub history: Vec<MetricOrderResultBackend>,
 }
