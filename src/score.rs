d17fa32a6acf6e70ef505ff31056999d7c6e780d
Frank Lyonnet <flyonnet@edamame.tech>
2024-06-03 10:52:50 +0200

    REFACTORE: score / health

--- src/score.rs
+++ src/score.rs
@@ -1,8 +1,9 @@
 use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 use serde_with::serde_as;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::error::Error;
+use tracing::trace;
 
 use crate::history::*;
 use crate::order::MetricOrderResult;
@@ -25,23 +26,6 @@ pub struct Score {
     pub metrics: ThreatMetrics,
     pub history: OrderHistory,
     pub last_compute: Option<DateTime<Utc>>,
-    pub compute_in_progress: bool,
-}
-
-pub trait ScoreTrait {
-    async fn compute(&mut self, compute_requested: bool) -> bool;
-    async fn get_progress_percent(&self) -> f32;
-    async fn get_last_completed_metric(&self) -> ThreatMetric;
-    fn get_metrics_summary(&self) -> String;
-    async fn get_history(&mut self) -> Result<OrderHistory, Box<dyn Error>>;
-    async fn remediate(&mut self, name: &str) -> Result<MetricOrderResult, Box<dyn Error>>;
-    async fn rollback(&mut self, name: &str) -> Result<MetricOrderResult, Box<dyn Error>>;
-    async fn update_threats(
-        &mut self,
-        platform: &str,
-        branch: &str,
-    ) -> Result<UpdateStatus, Box<dyn Error>>;
-    async fn threat_active(&mut self, name: &str) -> Result<bool, Box<dyn Error>>;
 }
 
 impl Score {
@@ -62,7 +46,167 @@ impl Score {
             metrics: ThreatMetrics::new(platform),
             history,
             last_compute: None,
-            compute_in_progress: false,
         }
     }
+    
+    pub async fn compute_score(&mut self) {
+        trace!("Starting score calculation");
+
+        let mut dim = HashMap::new();
+        dim.insert("network", (0, 0));
+        dim.insert("system services", (0, 0));
+        dim.insert("system integrity", (0, 0));
+        dim.insert("credentials", (0, 0));
+        dim.insert("applications", (0, 0));
+
+        // Compute score
+        for m in &self.metrics.metrics {
+            trace!(
+                "Computing score for metric: {:?} - dimension {:?} - status {:?}",
+                m.metric.name,
+                m.metric.dimension,
+                m.status
+            );
+            // Compute totals
+            let dimension: &str = &m.metric.dimension;
+            let (mut current, mut max) = dim[dimension];
+            max += m.metric.severity;
+            // Increment if not threat or not unknown
+            if matches!(m.status, ThreatStatus::Inactive) {
+                current += m.metric.severity;
+            }
+            // Insert back
+            dim.insert(&m.metric.dimension, (current, max));
+
+            // TODO: implement complex logic (amplifiers, exceptions) into a configurable scheme
+            // TODO: support metrics those type is beyond bool
+        }
+        let mut overall = 0;
+        let mut overall_max = 0;
+
+        let (current, max) = dim["network"];
+        overall_max += max;
+        overall += current;
+        if max > 0 {
+            self.network = 100 * current / max;
+        } else {
+            // Tell the UX to ignore this dimension
+            trace!("Network dimension is ignored");
+            self.network = -1;
+        }
+
+        let (current, max) = dim["system services"];
+        overall_max += max;
+        overall += current;
+        if max > 0 {
+            self.system_services = 100 * current / max;
+        } else {
+            // Tell the UX to ignore this dimension
+            trace!("System services dimension is ignored");
+            self.system_services = -1;
+        }
+
+        let (current, max) = dim["system integrity"];
+        overall_max += max;
+        overall += current;
+        if max > 0 {
+            self.system_integrity = 100 * current / max;
+        } else {
+            // Tell the UX to ignore this dimension
+            trace!("System integrity dimension is ignored");
+            self.system_integrity = -1;
+        }
+
+        let (current, max) = dim["credentials"];
+        overall_max += max;
+        overall += current;
+        if max > 0 {
+            self.credentials = 100 * current / max;
+        } else {
+            // Tell the UX to ignore this dimension
+            trace!("Credentials dimension is ignored");
+            self.credentials = -1;
+        }
+
+        let (current, max) = dim["applications"];
+        overall_max += max;
+        overall += current;
+        if max > 0 {
+            self.applications = 100 * current / max;
+        } else {
+            // Tell the UX to ignore this dimension
+            trace!("Applications dimension is ignored");
+            self.applications = -1;
+        }
+
+        if overall_max > 0 {
+            self.overall = 100 * overall / overall_max;
+        } else {
+            self.overall = 0;
+        }
+        self.stars = self.overall as f64 * 5.0 / 100.0;
+    }
+
+    // Compliance computation
+    pub async fn compute_compliance(&mut self) {
+        // List all tags in metrics, retain only their comma separated prefix
+        let mut tags: HashSet<String> = HashSet::new();
+
+        for m in &self.metrics.metrics {
+            for tag in &m.metric.tags {
+                if let Some(index) = tag.find(',') {
+                    let prefix = tag[0..index].to_string();
+                    tags.insert(prefix);
+                } else {
+                    tags.insert(tag.clone());
+                }
+            }
+        }
+
+        trace!("Compliance tags: {:?}", tags);
+
+        // Create a vector of compliance % per tag
+        let mut compliance: HashMap<String, f64> = HashMap::new();
+        // Scan through self.metrics.metrics and compute compliance for each tag
+        for tag in tags {
+            let mut total = 0;
+            let mut compliant = 0;
+            for m in &self.metrics.metrics {
+                for metric_tag in m.metric.tags.clone() {
+                    if metric_tag.starts_with(&tag) {
+                        total += 1;
+                        if matches!(m.status, ThreatStatus::Inactive) {
+                            compliant += 1;
+                        }
+                    }
+                }
+            }
+            if total > 0 {
+                let compliance_of_tag = (100.0 * compliant as f64) / total as f64;
+                trace!(
+                    "Compliance of tag {} is {} over {} which is {} %",
+                    tag,
+                    compliant,
+                    total,
+                    compliance_of_tag
+                );
+                compliance.insert(tag, compliance_of_tag);
+            }
+        }
+        // Write the compliance
+        self.compliance = compliance;
+    }
+}
+
+pub trait ScoreTrait {
+    async fn get_metrics_summary(&self) -> String;
+    async fn get_history(&self) -> Result<OrderHistory, Box<dyn Error>>;
+    async fn remediate(&self, name: &str) -> Result<MetricOrderResult, Box<dyn Error>>;
+    async fn rollback(&self, name: &str) -> Result<MetricOrderResult, Box<dyn Error>>;
+    async fn update_threats(
+        &mut self,
+        platform: &str,
+        branch: &str,
+    ) -> Result<UpdateStatus, Box<dyn Error>>;
+    async fn threat_active(&self, name: &str) -> Result<bool, Box<dyn Error>>;
 }
