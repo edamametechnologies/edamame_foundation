ea1864cd9724a0a427d6907eca81d70b6d895bc2
Frank Lyonnet <flyonnet@edamame.tech>
2024-07-01 14:10:42 +0200

    FIX: logger, updated threats

--- src/threat_metrics_ios.rs
+++ src/threat_metrics_ios.rs
@@ -340,12 +340,12 @@ pub static THREAT_METRICS_IOS: &str = r#"{
           {
             "locale": "EN",
             "class": "html",
-            "target": "<p>This feature allows you to scan your network to identify all connected devices. During the scan, the app checks for open ports and associated CVEs. You can then analyze these potential issues with the help of AI and decide if the threat should be ignored. Once all devices are deemed safe, this threat will be marked as inactive.</p>"
+            "target": "<p>Scan your network to identify all connected devices and assess potential threats.</p><h4>Steps to verify a critical device</h4><ul><li>Navigate to the 'Network' tab.</li><li>Devices of critical importance are marked with yellow for medium criticality and red for high criticality.</li><li>Select a critical device.</li><li>Assess each port's criticality by reading the associated CVEs and analyzing potential issues with AI.</li><li>If a port is determined to be safe, mark it as verified.</li></ul><p>Once all devices are deemed safe, this threat will be marked as inactive.</p>"
           },
           {
             "locale": "FR",
             "class": "html",
-            "target": "<p>Cette fonction vous permet de scanner votre réseau pour identifier tous les appareils connectés. Pendant le scan, l'application vérifie les ports ouverts et les CVE associés. Vous pouvez ensuite analyser ces problèmes potentiels avec l'aide de l'IA et décider si la menace doit être ignorée. Une fois que tous les appareils sont considérés comme sûrs, cette menace sera marquée comme inactive.</p>"
+            "target": "<p>Scannez votre réseau pour identifier tous les appareils connectés et évaluer les menaces potentielles.</p><h4>Étapes pour vérifier un appareil critique</h4><ul><li>Allez dans l'onglet 'Réseau'.</li><li>Les appareils de grande importance sont marqués en jaune pour une criticité moyenne et en rouge pour une criticité élevée.</li><li>Sélectionnez un appareil critique.</li><li>Évaluez la criticité de chaque port en lisant les CVE associés et en analysant les problèmes potentiels avec l'IA.</li><li>Si un port est déterminé comme sûr, marquez-le comme vérifié.</li></ul><p>Une fois que tous les appareils sont considérés comme sûrs, cette menace sera marquée comme inactive.</p>"
           }
         ]
       },
