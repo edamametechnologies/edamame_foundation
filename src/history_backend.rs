c37c852a0da52f49092eed1748e016bb19e68a19
Frank Lyonnet <flyonnet@edamame.tech>
2024-07-08 20:15:03 +0200

    REFACTOR: moved backend structures to edamame_backend

--- src/history_backend.rs
+++
@@ -1,7 +0,0 @@
-use crate::order_backend::*;
-use serde::{Deserialize, Serialize};
-
-#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, PartialOrd)]
-pub struct OrderHistoryBackend {
-    pub history: Vec<MetricOrderResultBackend>,
-}
