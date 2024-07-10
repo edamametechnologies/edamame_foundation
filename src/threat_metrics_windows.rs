5d8140cc579766447a64f0653ad40f230514e685
Frank Lyonnet <flyonnet@edamame.tech>
2024-07-10 16:32:35 +0200

    CHORES: updated metrics

--- src/threat_metrics_windows.rs
+++ src/threat_metrics_windows.rs
@@ -2,8 +2,8 @@
 pub static THREAT_METRICS_WINDOWS: &str = r#"{
   "name": "threat model Windows",
   "extends": "none",
-  "date": "April 18th 2024",
-  "signature": "aa2ea7ea0e72ad085c7c665fbb5d13b21463e236822f045383285c7659616709",
+  "date": "May 03th 2024",
+  "signature": "56d3ccf012b79b06adfe2e0e63db7c373a614cc672b958833ce51aa4ab26ec9a",
   "metrics": [
     {
       "name": "edamame helper disabled",
@@ -1399,7 +1399,9 @@ pub static THREAT_METRICS_WINDOWS: &str = r#"{
       "dimension": "network",
       "severity": 5,
       "scope": "generic",
-      "tags": ["CIS Benchmark Level 1,windows_security/smb1_protocol_disabled"],
+      "tags": [
+        "CIS Benchmark Level 1,windows_security/smb1_protocol_disabled"
+      ],
       "description": [
         {
           "locale": "EN",
