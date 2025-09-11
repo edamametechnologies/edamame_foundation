1811443974f8fa1cb4dd997326e7e8cf9123ab5d
EDAMAME Dev Local <dev@edamame.tech>
2025-09-11 13:56:05 +0200

    UPDATE: Replace `tonic_build` with `tonic_prost_build` in `build.rs` for improved protocol compilation. Update dependencies in `Cargo.lock` to newer versions, including `rustix` to `1.1.2`, `tonic` and `tonic-build` to `0.14.2`, and various other libraries for enhanced compatibility and performance. Adjust `Cargo.toml` to reflect these changes, ensuring alignment with the latest library versions.

--- build.rs
+++ build.rs
@@ -1,6 +1,6 @@
 fn main() {
     // Tonic/proto
-    match tonic_build::compile_protos("./proto/edamame.proto") {
+    match tonic_prost_build::compile_protos("./proto/edamame.proto") {
         Ok(_) => println!("Tonic/proto compiled successfully."),
         Err(e) => {
             panic!("Failed to compile Tonic/proto: {}", e);
